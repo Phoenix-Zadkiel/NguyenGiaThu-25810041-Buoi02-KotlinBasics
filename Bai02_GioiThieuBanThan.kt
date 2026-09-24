ba4d06f8950f2ad1c471df6de3ec873a2260f096
Nguyen Za Thu <nguyengiathu24052004@gmail.com>
2026-09-24 23:16:07 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,11 @@
+// Ho va ten: Nguyen Gia Thu
+// MSSV: Dien MSSV cua ban
+fun main() {
+    val hoTen: String = "Nguyen Gia Thu"
+    val namSinh: Int = 2004
+    val namHienTai: Int = 2026
+
+    val tuoi: Int = namHienTai - namSinh
+
+    println("Xin chao! Minh ten la $hoTen, nam nay minh $tuoi tuoi.")
+}
