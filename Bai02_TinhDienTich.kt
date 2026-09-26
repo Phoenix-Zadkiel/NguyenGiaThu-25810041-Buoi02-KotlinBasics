2d64c2ba2d766a3e0f6c64c46fe4c1da09466770
Nguyen Za Thu <nguyengiathu24052004@gmail.com>
2026-09-26 17:06:48 +0700

    Buoi03 Bai02 tinh dien tich

---
+++ Bai02_TinhDienTich.kt
@@ -0,0 +1,9 @@
+// Họ tên: Nguyen Gia Thụ
+// MSSV: 25810041
+
+fun tinhDienTich(chieuDai: Double, chieuRong: Double): Double {
+    return chieuDai * chieuRong
+}
+
+println(tinhDienTich(5.0, 3.0))
+println(tinhDienTich(7.5, 4.0))
