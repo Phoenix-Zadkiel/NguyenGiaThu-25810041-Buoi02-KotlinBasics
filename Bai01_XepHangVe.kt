27f6e3c180b97789148b1c07fff0a4670540fc62
Nguyen Za Thu <nguyengiathu24052004@gmail.com>
2026-09-26 16:54:36 +0700

    Buoi03 Bai01 xep hang ve

---
+++ Bai01_XepHangVe.kt
@@ -0,0 +1,14 @@
+// Họ tên: Nguyen Gia Thu
+// MSSV: 25810041
+
+val tuoi = 25
+
+val loaiVe = if (tuoi < 18) {
+    "Vé trẻ em"
+} else if (tuoi >= 60) {
+    "Ve cao tuoi"
+} else {
+    "Ve ngưoi lon"
+}
+
+println(loaiVe)
